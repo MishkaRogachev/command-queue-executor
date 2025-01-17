8a1d06cdf0fcdb7d74313fe25217dfda35a92e0d
MishkaRogachev <mishkarogachev@gmail.com>
2025-01-17 13:51:18 +0300

    chore: add a test case with two concurrent providers

--- pkg/producer/producer_test.go
+++ pkg/producer/producer_test.go
@@ -11,44 +11,67 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func mockServerHandler(msg string) string {
+	cmd, err := models.DeserializeWrapper(msg)
+	fmt.Println("Received command:", msg)
+	if err != nil {
+		return `{"success": false, "message": "failed to parse command"}`
+	}
+
+	switch cmd.Type {
+	case models.AddItem:
+		return `{"success": true, "message": "item added"}`
+	case models.DeleteItem:
+		return `{"success": true, "message": "item deleted"}`
+	case models.GetItem:
+		return `{"success": true, "value": "testValue1"}`
+	case models.GetAll:
+		return `{"success": true, "items": [{"key": "testKey1", "value": "testValue1"}]}`
+	default:
+		return `{"success": false, "message": "unknown command"}`
+	}
+}
+
 func TestProducerWithInprocMQ(t *testing.T) {
 	inprocMQ := mq.NewInprocMQ()
+
 	producer := NewProducer(inprocMQ, 1*time.Second)
 
-	// Set up a mock response handler to simulate a server
-	inprocMQ.ServeHandler((func(msg string) string {
-		cmd, err := models.DeserializeWrapper(msg)
-		fmt.Println("Received command:", msg)
-		if err != nil {
-			return `{"success": false, "message": "failed to parse command"}`
-		}
-
-		switch cmd.Type {
-		case models.AddItem:
-			return `{"success": true, "message": "item added"}`
-		case models.DeleteItem:
-			return `{"success": true, "message": "item deleted"}`
-		case models.GetItem:
-			return `{"success": true, "value": "testValue1"}`
-		case models.GetAll:
-			return `{"success": true, "items": [{"key": "testKey1", "value": "testValue1"}]}`
-		default:
-			return `{"success": false, "message": "unknown command"}`
-		}
-	}))
+	inprocMQ.ServeHandler(mockServerHandler)
 
 	fileName := "../../test_data/test_commands_short.txt"
 
-	// Use a WaitGroup to ensure all goroutines complete
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	go func() {
-		defer wg.Done()
-		err := producer.ReadCommandsFromFile(fileName)
-		assert.NoError(t, err)
-	}()
+	go runProducerTestWithFile(t, producer, fileName, &wg)
 
-	// Wait for all commands to be processed
+	wg.Wait()
+}
+
+func runProducerTestWithFile(t *testing.T, producer *Producer, fileName string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	err := producer.ReadCommandsFromFile(fileName)
+	assert.NoError(t, err)
+}
+
+func TestConcurrentProducersWithInprocMQ(t *testing.T) {
+	inprocMQ := mq.NewInprocMQ()
+
+	producer1 := NewProducer(inprocMQ, 1*time.Second)
+	producer2 := NewProducer(inprocMQ, 1*time.Second)
+
+	inprocMQ.ServeHandler(mockServerHandler)
+
+	fileName1 := "../../test_data/test_commands_medium.txt"
+	fileName2 := "../../test_data/test_commands_short.txt"
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go runProducerTestWithFile(t, producer1, fileName1, &wg)
+	go runProducerTestWithFile(t, producer2, fileName2, &wg)
+
 	wg.Wait()
 }
